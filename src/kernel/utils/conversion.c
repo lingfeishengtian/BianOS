5cbfb5220e88fb5c3145cff582820a87414c67ea
Hunter Han <hunter.han@gmail.com>
2020-08-17 22:54:56 -0500

    append 0x to hex

--- src/kernel/utils/conversion.c
+++ src/kernel/utils/conversion.c
@@ -3,17 +3,20 @@
 
 char* hex_to_ascii(unsigned int hex){
     unsigned char size = sizeof(hex) * 2;
-    char* res = (char*) kmalloc(size + 1); //32 bit -> 8 hex pairs
+    char* res = (char*) kmalloc(size + 3); //32 bit -> 8 hex pairs + 0x and null end
 
     for(int i = size - 1; i >= 0; --i){
         unsigned char ascii_val = hex & 0xF;
         if(ascii_val < 10) ascii_val += 48;
         else ascii_val += 55;
 
-        res[i] = ascii_val;
+        res[i + 2] = ascii_val;
         hex = hex >> 4;
     }
 
-    res[size] = 0;
+    res[0] = '0';
+    res[1] = 'x';
+
+    res[size + 2] = 0;
     return res;
 }
